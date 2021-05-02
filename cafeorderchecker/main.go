b68a1381e8ff27674305ec937ebe2c9715782475
Dinesh Tummlapalli <dinesh.tummlapalli@mrcooper.com>
2021-05-02 10:33:15 -0500

    cafe order checker

--- cafeorderchecker/main.go
+++ cafeorderchecker/main.go
@@ -22,7 +22,7 @@ func checkOrders(servedOrders []int, takeoutOrders []int, driveInOrders []int) {
 		v, ok := driveInOrdersIndex[servedOrders[i]].(int)
 		if ok {
 			if last_dinein_served_order_created_at > v {
-				fmt.Println("Order is not servered correctly\n")
+				fmt.Println("Order is not servered correctly")
 				break
 			} else {
 				last_dinein_served_order_created_at = v
